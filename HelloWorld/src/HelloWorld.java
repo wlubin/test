aee325c9e0d446e1c0e04f8a851a26302d38c577
wlubin <wlbin@outlook.com>
2017-06-12 22:06:25 +0800

    HelloWorld
    第一个项目提交

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.text.Format;
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HelloWorld!");
+		for (int i = 0; i < 10; i++) {
+			System.out.print(i);
+			System.out.println();
+		}
+	}
+
+}
